4e9d08701a523448c961bcfacd02e6e179fafa8d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "dns_caa_records" {
-  description = "All dns_caa_record resources"
-  value       = azurerm_dns_caa_record.dns_caa_records
-}
 output "dns_caa_records_fqdn" {
-  description = "List of fqdn values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.fqdn]
+  description = "Map of fqdn values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.fqdn }
 }
 output "dns_caa_records_name" {
-  description = "List of name values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.name]
+  description = "Map of name values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.name }
 }
 output "dns_caa_records_record" {
-  description = "List of record values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.record]
+  description = "Map of record values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.record }
 }
 output "dns_caa_records_resource_group_name" {
-  description = "List of resource_group_name values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.resource_group_name]
+  description = "Map of resource_group_name values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.resource_group_name }
 }
 output "dns_caa_records_tags" {
-  description = "List of tags values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.tags]
+  description = "Map of tags values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.tags }
 }
 output "dns_caa_records_ttl" {
-  description = "List of ttl values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.ttl]
+  description = "Map of ttl values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.ttl }
 }
 output "dns_caa_records_zone_name" {
-  description = "List of zone_name values across all dns_caa_records"
-  value       = [for k, v in azurerm_dns_caa_record.dns_caa_records : v.zone_name]
+  description = "Map of zone_name values across all dns_caa_records, keyed the same as var.dns_caa_records"
+  value       = { for k, v in azurerm_dns_caa_record.dns_caa_records : k => v.zone_name }
 }
 
